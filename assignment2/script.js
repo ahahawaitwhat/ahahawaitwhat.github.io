f5d8463af34400eee89234f187cbe85d261b1b97
ahahawaitwhat <wanming639@gmail.com>
2025-05-08 16:00:47 +1000

    week 9

--- assignment2/script.js
+++ assignment2/script.js
@@ -2,9 +2,12 @@ const audio = document.querySelector("#current-audio");
 const playPauseBtn = document.querySelector("#play-pause-btn");
 const playPauseImg = document.querySelector("#play-pause-img");
 const progressBar = document.querySelector("#progress-bar-fill");
+const currentDuration = document.querySelector("#current-duration");
+const remainingDuration = document.querySelector("#remaining-duration");
 audio.removeAttribute("controls");
 // playPauseBtn.addEventListener("click", togglePlayPause);
 audio.addEventListener("timeupdate", updateProgressBar);
+audio.addEventListener("timeupdate", updateCurrentDuration);
 function togglePlayPause() {
   if (audio.paused || audio.ended) {
     audio.play();
@@ -17,6 +20,31 @@ function togglePlayPause() {
 function updateProgressBar() {
   const value = (audio.currentTime / audio.duration) * 100;
   progressBar.style.width = value + "%";
+}
+function updateCurrentDuration() {
+  let integerTime = Math.floor(audio.currentTime);
+  console.log(integerTime);
+  if (integerTime < 10) {
+    let formattedTime = "0:0" + integerTime;
+    console.log(formattedTime);
+    document.getElementById("current-duration").innerHTML = formattedTime;
+  } else if (integerTime < 60) {
+    let formattedTime = "0:" + integerTime;
+    console.log(formattedTime);
+    document.getElementById("current-duration").innerHTML = formattedTime;
+  } else if (integerTime < 70) {
+    let secondsOnly = integerTime - 60;
+    console.log(secondsOnly);
+    let formattedTime = "1:0" + secondsOnly;
+    console.log(formattedTime);
+    document.getElementById("current-duration").innerHTML = formattedTime;
+  } else {
+    let secondsOnly = integerTime - 60;
+    console.log(secondsOnly);
+    let formattedTime = "1:" + secondsOnly;
+    console.log(formattedTime);
+    document.getElementById("current-duration").innerHTML = formattedTime;
+  }
 }
 
 let audiotrack = 1;
---
+++ week9/script.js
@@ -0,0 +1,22 @@
+const myButton = document.querySelector("#my-button");
+//myButton.addEventListener("click", onClick);
+myButton.onmousedown = function () {
+  //  onClick();
+};
+
+myButton.onmouseup = function () {
+  // offClick();
+};
+
+//function onClick() {
+//  myButton.style.backgroundColor = "blue";
+//  myButton.style.scale = "0.96";
+//}
+
+//function offClick() {
+//  myButton.style.backgroundColor = "rgb(59, 101, 179)";
+//  myButton.style.scale = "0.98";
+//}
+
+//
+//
