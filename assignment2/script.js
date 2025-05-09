2ac2a6f2a99060598e353cd14fe155eb2d4d7646
ahahawaitwhat <wanming639@gmail.com>
2025-05-09 19:12:50 +1000

    added simple track navigation

--- assignment2/script.js
+++ assignment2/script.js
@@ -2,12 +2,25 @@ const audio = document.querySelector("#current-audio");
 const playPauseBtn = document.querySelector("#play-pause-btn");
 const playPauseImg = document.querySelector("#play-pause-img");
 const progressBar = document.querySelector("#progress-bar-fill");
-const currentDuration = document.querySelector("#current-duration");
-const remainingDuration = document.querySelector("#remaining-duration");
 audio.removeAttribute("controls");
-// playPauseBtn.addEventListener("click", togglePlayPause);
-audio.addEventListener("timeupdate", updateProgressBar);
-audio.addEventListener("timeupdate", updateCurrentDuration);
+audio.addEventListener("ended", autoNextTrack);
+audio.addEventListener("timeupdate", handleTimeUpdate);
+
+//All time update functions
+function handleTimeUpdate() {
+  updateProgressBar();
+  updateCurrentDuration();
+  updateRemainingDuration();
+}
+
+//Play-plause functionality
+document.addEventListener("keydown", function (event) {
+  if (event.code === "Space" || event.key === " ") {
+    event.preventDefault();
+    togglePlayPause();
+  }
+});
+
 function togglePlayPause() {
   if (audio.paused || audio.ended) {
     audio.play();
@@ -17,36 +30,107 @@ function togglePlayPause() {
     playPauseImg.src = "media/icons8-play-64.png";
   }
 }
+
+//Set audio source & name
+function setAudioSrcName() {
+  if (audiotrack === 1) {
+    audio.src = "media/p-hase_Hes.mp3";
+    document.getElementById("track-name").innerHTML = "Hes";
+  } else if (audiotrack === 2) {
+    audio.src = "media/p-hase_Dry-Down-feat-Ben-Snaath.mp3";
+    document.getElementById("track-name").innerHTML =
+      "Dry Down (Ft. Ben Snaath)";
+  } else if (audiotrack === 3) {
+    audio.src = "media/p-hase_Leapt.mp3";
+    document.getElementById("track-name").innerHTML = "Leapt";
+  } else if (audiotrack === 4) {
+    audio.src = "media/p-hase_Water-Feature.mp3";
+    document.getElementById("track-name").innerHTML = "Water Feature";
+  }
+}
+
+//Next-previous track controls & autoplay
+function skipPrevious() {
+  if (audiotrack === 1) {
+    audiotrack = 4;
+  } else {
+    audiotrack--;
+  }
+  setAudioSrcName();
+  audio.play();
+  playPauseImg.src = "media/icons8-pause-64.png";
+}
+
+function skipNext() {
+  checkAuto = false;
+  nextTrack();
+}
+
+function nextTrack() {
+  if (audiotrack === 4) {
+    audiotrack = 1;
+  } else {
+    audiotrack++;
+  }
+  setAudioSrcName();
+  //check if loop is on
+  if (loop === false && checkAuto === true && audiotrack === 1) {
+    audio.pause();
+    playPauseImg.src = "media/icons8-play-64.png";
+    resetProgressBar();
+  } else {
+    audio.play();
+    playPauseImg.src = "media/icons8-pause-64.png";
+  }
+}
+
+function autoNextTrack() {
+  if (audio.ended === true) {
+    checkAuto = true;
+    nextTrack();
+  }
+}
+
+//Progress bar
 function updateProgressBar() {
   const value = (audio.currentTime / audio.duration) * 100;
   progressBar.style.width = value + "%";
 }
-function updateCurrentDuration() {
-  let integerTime = Math.floor(audio.currentTime);
-  console.log(integerTime);
-  if (integerTime < 10) {
-    let formattedTime = "0:0" + integerTime;
-    console.log(formattedTime);
-    document.getElementById("current-duration").innerHTML = formattedTime;
-  } else if (integerTime < 60) {
-    let formattedTime = "0:" + integerTime;
-    console.log(formattedTime);
-    document.getElementById("current-duration").innerHTML = formattedTime;
-  } else if (integerTime < 70) {
-    let secondsOnly = integerTime - 60;
-    console.log(secondsOnly);
-    let formattedTime = "1:0" + secondsOnly;
-    console.log(formattedTime);
-    document.getElementById("current-duration").innerHTML = formattedTime;
-  } else {
-    let secondsOnly = integerTime - 60;
-    console.log(secondsOnly);
-    let formattedTime = "1:" + secondsOnly;
-    console.log(formattedTime);
-    document.getElementById("current-duration").innerHTML = formattedTime;
-  }
+
+function resetProgressBar() {
+  progressBar.style.width = "0%";
 }
 
-let audiotrack = 1;
+//Display current & remaining durations
+function updateCurrentDuration() {
+  document.getElementById("current-duration").innerHTML = formatTime(
+    audio.currentTime
+  );
+}
 
-// Add other functionalities here
+function updateRemainingDuration() {
+  let remainingTime = audio.duration - audio.currentTime;
+  document.getElementById("remaining-duration").innerHTML =
+    "-" + formatTime(remainingTime);
+}
+
+function formatTime(timeToFormat) {
+  let integerTime = Math.floor(timeToFormat);
+  if (integerTime < 10) {
+    formattedTime = "0:0" + integerTime;
+  } else if (integerTime < 60) {
+    formattedTime = "0:" + integerTime;
+  } else if (integerTime < 70) {
+    let secondsOnly = integerTime - 60;
+    formattedTime = "1:0" + secondsOnly;
+  } else {
+    let secondsOnly = integerTime - 60;
+    formattedTime = "1:" + secondsOnly;
+  }
+  return formattedTime;
+}
+
+var audiotrack = 1;
+var loop = false;
+var checkAuto = true;
+console.log(audiotrack);
